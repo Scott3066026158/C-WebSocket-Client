e5d0cb64545f829f9bbf475c10ab04f56ce3c8f6
Scott3066026158 <scott3066026158@gmail.com>
2020-06-11 10:38:29 +0800

    delete NetDisk_Qt

--- NetDisk_Qt/NetDisk/crypto/base32_bch.h
+++
@@ -1,8 +0,0 @@
-#ifndef __H_BASE32_BCH_H__
-#define __H_BASE32_BCH_H__
-
-char* base32_encode(uint8_t* data, int datalen);
-
-#endif // !1
-
-
--- NetDisk_Qt/NetDisk/crypto/bch.h
+++
@@ -1,8 +0,0 @@
-#ifndef __H_BCH_H__
-#define __H_BCH_H__
-#pragma warning(disable:4996)
-
-void btc_to_bch(char addrStr[35]);
-char* encode(char prefix[], char tp[], uint8_t hh[20]);
-#endif
-
